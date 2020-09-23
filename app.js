9bcb9c8b6cf7b3c45be20bd2d6628a124c7a81ba
Dilyara <dilyara_1322@mail.ru>
2020-09-23 15:26:25 +0300

    complicated function

--- app.js
+++ app.js
@@ -46,20 +46,28 @@ $(function(){
     let slidesCount = $(".slideJS").length;
     let currentSlide = $(".slide--current");
     let currentSlideIndex = currentSlide.data("index");
-    $(".slide-button.next").on("click", function(event){
-        event.preventDefault();
+
+    let incCurrentIndex = function(currentSlideIndex, slidesCount){
+        return (currentSlideIndex + 1) % slidesCount;
+    };
+    let decCurrentIndex = function(currentSlideIndex, slidesCount){
+        currentSlideIndex = currentSlideIndex - 1;
+        if (currentSlideIndex < 0) currentSlideIndex = slidesCount - 1;
+        return currentSlideIndex;
+    };
+    function changeCurrentSlide(changeCurrentIndex){
         currentSlide.removeClass("slide--current");
-        currentSlideIndex = (currentSlideIndex + 1) % slidesCount;
+        currentSlideIndex = changeCurrentIndex(currentSlideIndex, slidesCount);
         $(".slideJS[data-index="+currentSlideIndex+"]").addClass("slide--current");
         currentSlide = $(".slide--current");
+    };
+    
+    $(".slide-button.next").on("click", function(event){
+        event.preventDefault();
+        changeCurrentSlide(incCurrentIndex);
     });
     $(".slide-button.prev").on("click", function(event){
         event.preventDefault();
-        currentSlide.removeClass("slide--current");
-        currentSlideIndex = currentSlideIndex - 1;
-        if (currentSlideIndex < 0) currentSlideIndex = slidesCount - 1;
-        $(".slideJS[data-index="+currentSlideIndex+"]").addClass("slide--current");
-        currentSlide = $(".slide--current");
+        changeCurrentSlide(decCurrentIndex);
     });
-    
 });
